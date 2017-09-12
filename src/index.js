89c50b5045f958a42924631ca0a4e19c39366556
Pierre Olivier <firzenax@gmail.com>
2017-09-13 01:30:17 +0200

    Day 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const App = function() {
+	return <div>Coding youtube, day 1 :)</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
